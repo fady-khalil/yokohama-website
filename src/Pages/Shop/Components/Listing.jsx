cbf10805b56230eeb4a22af4cecc790c3dbc5f38
fady-khalil <fady.khalil123@hotmail.com>
2024-10-04 12:35:28 +0300

    edit pag

--- src/Pages/Shop/Components/Listing.jsx
+++ src/Pages/Shop/Components/Listing.jsx
@@ -9,10 +9,8 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   const createPageArray = () => {
     let pages = [];
     if (totalPages <= 5) {
-      // If there are 5 or fewer pages, show all pages
       pages = Array.from({ length: totalPages }, (_, index) => index + 1);
     } else {
-      // Show page numbers with ellipses
       if (currentPage <= 3) {
         pages = [1, 2, 3, 4, totalPages];
       } else if (currentPage >= totalPages - 2) {
@@ -27,9 +25,7 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
       } else {
         pages = [1, currentPage - 1, currentPage, currentPage + 1, totalPages];
       }
-      // Remove duplicates
       pages = [...new Set(pages)];
-      // Add ellipses if needed
       if (pages[1] > 2) pages.splice(1, 0, "...");
       if (pages[pages.length - 2] < totalPages - 1)
         pages.splice(pages.length - 1, 0, "...");
@@ -44,7 +40,7 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
       <button
         onClick={() => onPageChange(currentPage - 1)}
         disabled={currentPage === 1}
-        className="mx-2 px-4 py-2 rounded bg-gray-200 text-gray-700 disabled:opacity-50"
+        className="mx-2 px-2 sm:px-4 py-1 sm:py-2 rounded bg-gray-200 text-gray-700 disabled:opacity-50"
       >
         &lt;
       </button>
@@ -52,7 +48,7 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
         <button
           key={index}
           onClick={() => typeof page === "number" && onPageChange(page)}
-          className={`mx-2 px-4 py-2 rounded ${
+          className={`mx-1 sm:mx-2 px-2 sm:px-4 py-1 sm:py-2 rounded ${
             typeof page === "number" && page === currentPage
               ? "bg-primary text-white"
               : "bg-gray-200 text-gray-700"
@@ -64,7 +60,7 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
       <button
         onClick={() => onPageChange(currentPage + 1)}
         disabled={currentPage === totalPages}
-        className="mx-2 px-4 py-2 rounded bg-gray-200 text-gray-700 disabled:opacity-50"
+        className="mx-2 px-2 sm:px-4 py-1 sm:py-2 rounded bg-gray-200 text-gray-700 disabled:opacity-50"
       >
         &gt;
       </button>
