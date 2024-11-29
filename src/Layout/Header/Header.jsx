3bf717fcd3417342a4c07d2e0a39a05daeac8a06
fady-khalil <fady.khalil123@hotmail.com>
2024-11-29 15:18:39 +0200

    adding new logo

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import Drawer from "./Drawer/Drawer";
 // inner
 import Container from "Components/Container/Container";
@@ -7,6 +7,7 @@ import Logo from "./Logo/Logo";
 import DesktopNav from "./Navigation/DesktopNav";
 import { List } from "@phosphor-icons/react";
 import logo from "assests/logo/marwan-2-logo.jpg";
+import { ModalContext } from "context/Auth/ModalContext";
 
 const Header = () => {
   const [drawerIsActive, setDrawerIsActive] = useState(false);
@@ -16,8 +17,16 @@ const Header = () => {
   const closeDrawerHandler = () => {
     setDrawerIsActive(false);
   };
+
+  const { openDealerModalHandeler } = useContext(ModalContext);
   return (
     <header className="sticky top-0 z-50 shadow-xl bg-white">
+      <div className="bg-dark text-white py-2.5 flex-1 min-w-full px-[1.6rem] lg:px-[3rem] ">
+        <div className="xl:px-[2rem] xxl:px-[4rem] flex items-center justify-between gap-x-4 uppercase font-medium text-sm ">
+          <Link>Login</Link>
+          <button onClick={openDealerModalHandeler}>Dealer login</button>
+        </div>
+      </div>
       <div className="xl:px-[2rem] xxl:px-[4rem]">
         <div className=" flex items-center justify-between w-full">
           {/* logo */}
--- src/Pages/Home/Sections/Brands/Brands.jsx
+++ src/Pages/Home/Sections/Brands/Brands.jsx
@@ -7,26 +7,16 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Pagination, Navigation, Autoplay } from "swiper/modules";
 import "./style.css";
-import brand1 from "assests/brand/brand-1.jpg";
-import brand2 from "assests/brand/brand-2.png";
-import brand3 from "assests/brand/brand-3.png";
-import brand4 from "assests/brand/brand-4.jpg";
-import brand5 from "assests/brand/brand-5.jpg";
-import brand6 from "assests/brand/brand-6.jpg";
-import brand7 from "assests/brand/brand-7.jpg";
-import brand8 from "assests/brand/brand-8.jpg";
+import brand1 from "assests/brand/1.png";
+import brand2 from "assests/brand/2.png";
+import brand3 from "assests/brand/3.png";
+import brand4 from "assests/brand/4.png";
+import brand5 from "assests/brand/5.png";
+import brand6 from "assests/brand/6.png";
+import brand7 from "assests/brand/7.png";
 
 const Brands = ({ noSpace }) => {
-  const images = [
-    brand1,
-    brand2,
-    brand3,
-    brand4,
-    brand5,
-    brand6,
-    brand7,
-    brand8,
-  ];
+  const images = [brand1, brand2, brand3, brand4, brand5, brand6, brand7];
   return (
     <section
       className={`${noSpace ? "mt-mega" : "pt-mega bg-dark pb-primary"}  `}
