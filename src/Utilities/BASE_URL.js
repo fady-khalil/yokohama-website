b16f423f2d5a782055d0010910cf9d88fba768b7
fady-khalil <fady.khalil123@hotmail.com>
2025-07-01 11:25:30 +0300

    adding new endpoint

--- src/Utilities/BASE_URL.js
+++ src/Utilities/BASE_URL.js
@@ -1,5 +1,5 @@
 const BASE_URL =
-  "https://erpxcloud-yokohamav3-18-06-2025-21345796.dev.odoo.com";
+  "https://erpxcloud-yokohamav3-18-06-2025-21603556.dev.odoo.com";
 // const BASE_URL = "https://erpxcloud-yokohamav3-dowgroup-17801640.dev.odoo.com";
 
 export default BASE_URL;
